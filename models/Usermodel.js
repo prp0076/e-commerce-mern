74d6bdfe441e9b05bf7c4eca9dca832f03183044
pushpraj paroha <satyamparoha786@gmail.com>
2023-05-09 18:43:40 +0530

    usermodel

---
+++ models/Usermodel.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema();
+const UserSchema= new Schema({
+    name:{
+        type:String,
+        required:true,
+        trim:true //remove whitespace
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+    phone:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:Number,
+        default:0
+    }
+},{timestamps:true})
+module.exports = mongoose.model("eusers",UserSchema)
