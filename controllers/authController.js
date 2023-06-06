bcc902ce484beaddf705c0aee772d1c8a2aec944
pushpraj paroha <satyamparoha786@gmail.com>
2023-06-06 20:28:30 +0530

    jwt

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
  const Usermodel = require("../models/Usermodel")
- const {hashPassword} = require("../helpers/authHelper")
+ const JWT= require("jsonwebtoken")
+ const {hashPassword, comparedPassword} = require("../helpers/authHelper")
  const registerController =async (req, res)=>{
   try {
     const {name,email,password,phone,address}=req.body;
@@ -46,4 +47,55 @@
     })
   }
 }
-module.exports = registerController
+
+
+//login controller
+const loginController =async(req,res)=>{
+try {
+    const {email,password}=req.body
+    if(!email || !password){
+        return res.status(404).send({
+            success:false,
+            message:'Invalid email or password'
+        })
+    }
+    //check user for compare password
+    const user = await Usermodel.findOne({email});
+    if(!user){
+        return res.status(404).send({
+            success:false,
+            message:'Email is not registered',
+            error
+        })
+    }
+    const match= await comparedPassword(password,user.password)
+    if(!match){
+        return res.status(200).send({
+            success:false,
+            message:'Invalid Password'
+        })
+    }
+
+    //token
+    const token= await JWT.sign({_id:user._id},process.env.JWT_SECRET,{expiresIn:'7d'});
+    res.status(200).send({
+        success:true,
+        message:'login succefully',
+        user:{
+            name:user.name,
+            email:user.email,
+            phone:user.phone,
+            address:user.address
+        },
+        token
+    });
+} catch (error) {
+  console.log(error);
+  res.status(500).send({
+    success:false,
+    message:'Error in login',
+    error
+  })    
+}
+}
+module.exports = {registerController,loginController}
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,14 @@
 const express = require("express");
 const router=express.Router();
 const registerController = require("../controllers/authController")
+const loginController = require("../controllers/authController");
 
 //routing || method:POST||register
 
 router.post("/register",registerController)
 
-//login controller
+//login controller //post 
+
+
+router.post('/login',loginController);
 module.exports= router
