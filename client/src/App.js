5440f624e0609dcb32fe7ae67f113acf01b3f36d
pushpraj paroha <satyamparoha786@gmail.com>
2023-06-14 15:16:56 +0530

    frontend

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Layout from "./Components/Layout"
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 function App() {
   return (
     <>
