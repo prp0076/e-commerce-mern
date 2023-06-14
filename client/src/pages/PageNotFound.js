817c8d35fcf98e5201c09f299c0883d834fecb02
pushpraj paroha <satyamparoha786@gmail.com>
2023-06-15 01:13:11 +0530

    pnf

--- client/src/pages/PageNotFound.js
+++ client/src/pages/PageNotFound.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import Layout from '../Components/Layout'
-
+import { Link } from 'react-router-dom'
 function PageNotFound() {
   return (
-     <Layout>
-        <h1>PageNotFound</h1>
-     </Layout>
+   <Layout title={"go back- page not found"}>
+   <div className="pnf">
+     <h1 className="pnf-title">404</h1>
+     <h2 className="pnf-heading">Oops ! Page Not Found</h2>
+     <Link to="/" className="pnf-btn">
+       Go Back
+     </Link>
+   </div>
+   </Layout>
     )
 }
 
