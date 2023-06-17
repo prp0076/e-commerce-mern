d16d90c987ea5dcec8a4280a94920254a0dcab0e
pushpraj paroha <satyamparoha786@gmail.com>
2023-06-17 20:30:48 +0530

    jwt

---
+++ client/src/Components/AdminMenu.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AdminMenu = () => {
+  return (
+    <div>AdminMenu</div>
+  )
+}
+
+export default AdminMenu
--- middleware/isAdmin.js
+++ middleware/isAdmin.js
@@ -2,10 +2,12 @@ const userModel=require("../models/Usermodel")
 const IsAdmin= async(req,res,next)=>{
     try {
         const user = await userModel.findById(req.user._id);
-        if(user.role == 1){
+        if(user.role !== 1){
+            console.log("aunathorized access");
+        }
+        else{
             next();
         }
-        
     } catch (error) {
         console.log(error);
         res.status(404).send({
